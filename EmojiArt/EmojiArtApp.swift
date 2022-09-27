8d819d328e14edf1d97db8e5338e59da9d5873f2
chlwpghks95 <chlwpghks95@awasoft.co.kr>
2022-09-27 14:43:31 +0900

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by 최제환 on 2022/09/27.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
