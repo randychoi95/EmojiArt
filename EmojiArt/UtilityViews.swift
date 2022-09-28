ffbdb4f554f82018b90a2472cd64ec3b9c4e8f57
chlwpghks95 <chlwpghks95@awasoft.co.kr>
2022-09-28 09:25:27 +0900

    :art: MVVM 설계
    - Model과 ViewModel(EmojiArtDocument) 추가

---
+++ EmojiArt/UtilityViews.swift
@@ -0,0 +1,45 @@
+//
+//  UtilityViews.swift
+//  EmojiArt
+//
+//  Created by 최제환 on 2022/09/27.
+//
+
+import SwiftUI
+
+struct OptionalImage: View {
+    var uiImage: UIImage?
+    
+    var body: some View {
+        if uiImage != nil {
+            Image(uiImage: uiImage!)
+        }
+    }
+}
+
+struct AnimatedActionButton: View {
+    var title: String? = nil
+    var systemImage: String? = nil
+    let action: () -> Void
+    
+    var body: some View {
+        Button {
+            withAnimation {
+                action()
+            }
+        } label: {
+            if title != nil && systemImage != nil {
+                Label(title!, systemImage: systemImage!)
+            } else if title != nil {
+                Text(title!)
+            } else if systemImage != nil {
+                Image(systemName: systemImage!)
+            }
+        }
+    }
+}
+
+struct IdentifiableAlert: Identifiable {
+    var id: String
+    var alert: () -> Alert
+}
